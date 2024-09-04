37cc8b09cdae9a28d93544d63e2ea970d86ab99a
SteevoMcCool <96311500+SteevoMcCool@users.noreply.github.com>
2024-09-03 22:27:19 -0400

    net

--- netlify/functions/hello-world/hello-world.js
+++ netlify/functions/hello-world/hello-world.js
@@ -1,14 +1,22 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+const zones = {
+  babbio:{roomMain:[]},
+  UCC: {roomMain:[ [0,"line", 0, 0, 10, 10, 3, "red" ]]},
+  Howe : {roomMain:[]},  
+}
+
 const handler = async (event) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
-    console.log("ello gobna")
+    const zone = event.queryStringParameters.zoneName || "UCC"
+    const roomNumber =  `room${Number(event.queryStringParameters.roomNumber) || "Main"}`
+    const from = Number(event.queryStringParameters.from || 0)
+    
+    let ret = zones[zone][roomNumber].filter(edit=> edit[0] >=from);
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      body: JSON.stringify(ret),
+     
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
--- smartInputs.js
+++ smartInputs.js
@@ -66,3 +66,67 @@ class Slider {
 
 
 
+class Radio {
+    constructor(choices,inline){
+        this.DOM = document.createElement((inline && "span") || "div")
+        this.DOM.className = "Radio"
+        this.DOM.innerHTML =   `<input type="number" min="${min}" max="${max}" value="${val}">
+                                <input type="range"  min="${min}" max="${max}" value="${val}">;`
+
+        this._min = min
+        this._max = max
+        this._value = val
+
+        this.numInput = this.DOM.querySelector("input[type=number]");
+        this.ranInput = this.DOM.querySelector("input[type=range]");
+
+        let eta = this
+        this.numInput.onchange= function(){
+            eta.value = eta.numInput.value
+
+        }
+
+        this.ranInput.oninput = function(){
+            eta.value = eta.ranInput.value
+        }
+    }
+    get max(){
+        return this._max
+    }
+    set max(newVal){
+        this.numInput.max = newVal
+        this.ranInput.max = newVal
+        this._max = max
+    }
+
+    get min(){
+        return this._min
+    }
+    set min(newVal){
+        this.numInput.min = newVal
+        this.ranInput.min = newVal
+        this._min = max
+    }
+
+    get value(){
+        return this._value
+    }
+    set value(newVal){
+        this.numInput.value = newVal
+        this.ranInput.value = newVal
+        this._value = newVal
+
+        if (this.link) {
+            this.link[0] [this.link[1]] = this._value
+            console.log(this.link)
+
+        }
+    }
+
+    setLink(object,prop){
+
+        this.link = [object,prop]
+        console.log(this.link)
+        this.value = this.value
+    }   
+}
